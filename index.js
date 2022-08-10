6f7473edd36dddf62e91ee0808e9a4cfd0d98bab
Anshul Pant <anshulpant14@gmail.com>
2022-08-10 16:42:45 +0530

    Add files via upload
    initial backend by anshul pant

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { exec } = require("child_process");
+const { stdout, stderr } = require('process');
+
+
+const app = express();
+
+
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+'/container.html');
+    // res.send("hi");
+});
+
+app.get("/run",(req,res)=>{
+    const cname = req.query.cname;
+    const cimage= req.query.cimage;
+      // console.log(cname +" "+cimage);
+//    res.send(cname+" "+cimage);
+     exec(`docker run -dit --name ${cname} ${cimage}` ,(err,stdout,stderr)=>{
+        console.log(stdout);
+        res.send("<pre>"+stdout+"</pre>");
+     })
+});
+app.get("/ps",(req,res)=>{
+//   exec("docker ps -a | tail -n +2 | awk '{print $2,$7, $10}'",(err,stdout,stderr)=>{
+//     console.log(stdout);
+//     res.send("<pre>"+stdout+"</pre>");
+    exec("docker ps | tail -n +2",(err,stdout,stderr)=>{
+    let a = stdout.split("\n");
+     res.write("<table border='5' align='center' width='100%'>");
+     res.write("<tr> <th>CONTAINER ID</th><th>IMAGE</th><th>COMMAND</th><th>CREATED</th><th>STATUS</th><th>NAME</th>");  
+     a.forEach((cdetail) => {
+         cinfo = cdetail.trim().split(/\s+/);
+        //  console.log(cinfo[2]);
+         res.write("<tr>"+" <td>"+cinfo[0]+"</td> "+"<td>"+cinfo[1]+"</td>"+"<td>"+cinfo[2]+"</td>"+"<td>"+cinfo[3]+" "+cinfo[4]+" "+cinfo[5]+"</td>"+"<td>"+cinfo[6]+" "+cinfo[7]+" "+cinfo[8]+"</td>"+"<td>"+cinfo[9]+"</td>"+"</tr>")
+     });
+       res.write("</table>");
+       res.send();
+    })
+    // res.send();
+})
+
+app.listen(3000,()=>{console.log("Container app tool started .....")});
+
+//http://localhost:3000/run?cname=os8&cimage=ubuntu  `localhost:3000/run?cname=${cn}&cimage=ubuntu`
