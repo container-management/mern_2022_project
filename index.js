34ab779557d8d6ef7b4a9d0845e0492a4e069ead
Anshul Pant <anshulpant14@gmail.com>
2022-08-12 12:08:04 +0530

    Update index.js
    added functionality of to get all the container and  some minor bug fix by anshul pant

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const { stdout, stderr } = require('process');
 const app = express();
 
 
-
+// function to send home page
 app.get("/",(req,res)=>{
   res.sendFile(__dirname+'/container.html');
     // res.send("hi");
 });
 
+// function to launch container(os) inside docker
 app.get("/run",(req,res)=>{
     const cname = req.query.cname;
     const cimage= req.query.cimage;
@@ -22,23 +23,60 @@ app.get("/run",(req,res)=>{
         res.send("<pre>"+stdout+"</pre>");
      })
 });
-app.get("/ps",(req,res)=>{
-//   exec("docker ps -a | tail -n +2 | awk '{print $2,$7, $10}'",(err,stdout,stderr)=>{
-//     console.log(stdout);
-//     res.send("<pre>"+stdout+"</pre>");
-    exec("docker ps | tail -n +2",(err,stdout,stderr)=>{
+
+// function to print detail of all the runing container
+app.get("/ps", (req, res) => {
+  //   exec("docker ps -a | tail -n +2 | awk '{print $2,$7, $10}'",(err,stdout,stderr)=>{
+  //     console.log(stdout);
+  //     res.send("<pre>"+stdout+"</pre>");
+  exec("docker ps | tail -n +2", (err, stdout, stderr) => {
     let a = stdout.split("\n");
-     res.write("<table border='5' align='center' width='100%'>");
-     res.write("<tr> <th>CONTAINER ID</th><th>IMAGE</th><th>COMMAND</th><th>CREATED</th><th>STATUS</th><th>NAME</th>");  
-     a.forEach((cdetail) => {
-         cinfo = cdetail.trim().split(/\s+/);
-        //  console.log(cinfo[2]);
-         res.write("<tr>"+" <td>"+cinfo[0]+"</td> "+"<td>"+cinfo[1]+"</td>"+"<td>"+cinfo[2]+"</td>"+"<td>"+cinfo[3]+" "+cinfo[4]+" "+cinfo[5]+"</td>"+"<td>"+cinfo[6]+" "+cinfo[7]+" "+cinfo[8]+"</td>"+"<td>"+cinfo[9]+"</td>"+"</tr>")
-     });
-       res.write("</table>");
-       res.send();
-    })
-    // res.send();
+     let alength = a.length;
+
+    res.write("<table border='5' align='center' width='100%'>");
+    res.write("<tr> <th>CONTAINER ID</th><th>IMAGE</th><th>COMMAND</th><th>CREATED</th><th>STATUS</th><th>NAME</th></tr>");
+    a.forEach((cdetail,index) => {
+     if(index<alength-1) {cinfo = cdetail.trim().split(/\s+/);
+      //  console.log(cinfo[2]);
+      res.write("<tr>" + " <td>" + cinfo[0] + "</td> " + "<td>" + cinfo[1] + "</td>" + "<td>" + cinfo[2] + "</td>" + "<td>" + cinfo[3] + " " + cinfo[4] + " " + cinfo[5] + "</td>" + "<td>" + cinfo[6] + " " + cinfo[7] + " " + cinfo[8] + "</td>" + "<td>" + cinfo[9] + "</td>" + "</tr>")
+      } 
+    });
+    res.write("</table>");
+    res.send();
+  })
+  // res.send();
+})
+
+//function to show all the container
+app.get("/psa", (req, res) => {
+  //   exec("docker ps -a | tail -n +2 | awk '{print $2,$7, $10}'",(err,stdout,stderr)=>{
+  //     console.log(stdout);
+  //     res.send("<pre>"+stdout+"</pre>");
+  exec("docker ps -a| tail -n +2", (err, stdout, stderr) => {
+    // console.log(stdout);
+    let a = stdout.split("\n");
+     let alength = a.length;
+    // console.log("a element"+alength); 
+    res.write("<table border='5' align='center' width='100%'>");
+    res.write("<tr> <th>CONTAINER ID</th><th>IMAGE</th><th>COMMAND</th><th>CREATED</th><th>STATUS</th><th>NAME</th></tr>");
+    a.forEach((cdetail, index) => {
+      //  console.log(index);
+      if (index < alength - 1) {
+        cinfo = cdetail.trim().split(/\s+/);
+        clength = cinfo.length;
+        // console.log(clength);
+        if(clength == 10){
+          res.write("<tr>" + " <td>" + cinfo[0] + "</td> " + "<td>" + cinfo[1] + "</td>" + "<td>" + cinfo[2] + "</td>" + "<td>" + cinfo[3] + " " + cinfo[4] + " " + cinfo[5] + "</td>" + "<td>" + " "+cinfo[clength-4] + " " + cinfo[clength-3] + " " + cinfo[clength-2] + "</td>" + "<td>" + cinfo[clength-1] + "</td>" + "</tr>")
+        }
+        else{
+           res.write("<tr>" + " <td>" + cinfo[0] + "</td> " + "<td>" + cinfo[1] + "</td>" + "<td>" + cinfo[2] + "</td>" + "<td>" + cinfo[3] + " " + cinfo[4] + " " + cinfo[5] + "</td>" + "<td>" + " " + cinfo[clength-6] + " " + cinfo[clength-5] +" "+cinfo[clength-4] + " " + cinfo[clength-3] + " " + cinfo[clength-2] + "</td>" + "<td>" + cinfo[clength-1] + "</td>" + "</tr>")
+            }
+      }
+    });
+    res.write("</table>");
+    res.send();
+  })
+  // res.send();
 })
 
 //function to remove all the containers
@@ -69,7 +107,7 @@ app.get("/search",(req,res)=>{
      exec(`docker images | grep ${cimage}` ,(err,stdout,stderr)=>{
         console.log(stdout);
            if(stdout==""){
-             stdout = cimage+"image not found";
+             stdout = cimage+" image not found";
               }
         res.send("<pre>"+stdout+"</pre>");
      })
